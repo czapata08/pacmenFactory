a2c0f9b7ca4ddb12775b8baae051cb79c5767da8
Carlos Zapata <carloszapata@Carloss-MBP.cable.rcn.com>
2022-06-03 16:29:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+<h1>PacMen Factory</h1>
+<h4>This projects uses a combination of javascript timers and DOM manipulation techniques to dynamically create PacMen and move within a defined container. </h4>
+
+<h2>Usage</h2>
+<li>Use bottom to create PacMen and move bottom to make them move and increase PacMen speed.<li>
+
+<h2>Roadmap</h2>
+<li>Create a counter that tracks each time that individual PacMen collide and display it to the user</li>
+
+
+<h2>Project Goals</h2>
+<ol> 
+    <li>Work with css and DOM to display PacMen</li>
+    <li>Work with Javascript timers to move pacmen within container</li>
+</ol>
+
+<h2> Key Learnings </h2>
+<ol> 
+   <li> Structure functions with timers to update data of a DOM element</li>
+</ol>
+
+<h2> Languages </h2>
+<li>JavaScript</li>
+<li>CSS</li>
+<li>HTML</li>
+<li>DOM</li>
+
+<h2> Author </h2> 
+<h3> Carlos Zapata </h3>
+<a href="https://github.com/czapata08">MyGitHub</a>
+
+<h2>MIT License</h2>
+<h3>
+Copyright (c) 2020 John Williams x Carlos Zapata
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</h3>
+
+<h2> Thanks for reading! </h2>
---
+++ pacmen.js
@@ -0,0 +1,94 @@
+ //Image array for PacMen 
+    const pacArray = [
+        ['PacMan1.png', 'PacMan2.png'],
+        ['PacMan3.png', 'PacMan4.png']
+    ];
+  
+//Container Element Div
+    let container = document.getElementById("container");
+    
+ //Container Element Position 
+    let p = container.getBoundingClientRect();
+     console.log(p)
+
+//Min + Max position for X & Y axis
+     var minX = p.left - 20;
+     var maxX = p.right - 20;
+     var minY = p.top - 20;
+     var maxY = p.bottom - 20;
+
+    var pos = 0;
+    var direction = 0;
+    const pacMen = []; // This array holds all the pacmen
+  
+//Select A Random Image
+    function randomImage(pacArray) {
+        return pacArray[Math.floor(Math.random() * 2)][Math.floor(Math.random() * 2)]
+        };
+
+    function random (min, max) {
+        return Math.random() * (max - min) + min
+    }
+
+    function setToRandom(dx,dy) {
+        return {
+            dx: random(minX, maxX),
+            dy: random(minY, maxY),
+        }
+    }
+    // Factory to make a PacMan at a random position with random velocity
+    function makePac() {
+        // returns an object with random values scaled {x: 33, y: 21}
+        let velocity = {
+            x:(random(10, 50)),
+            y:(random(12, 40))
+        }
+        let position = setToRandom();
+        // Add image to div id = game
+        let container = document.getElementById('container');
+        let newimg = document.createElement('img');
+        // set position here 
+        newimg.style.left = position.dx + 'px';
+        newimg.style.top = position.dy + 'px';
+        newimg.src = randomImage(pacArray);
+        newimg.width = 50;
+        
+    
+
+        // add new Child image to game
+        container.appendChild(newimg);
+        // return details in an object
+        return {
+            position,
+            velocity,
+            newimg
+        }
+    }
+
+    function update() {
+        //Loop over each pacman in array
+        pacMen.forEach((item) => {
+        checkCollisions(item);
+        item.position.dx += item.velocity.x;
+        item.position.dy += item.velocity.y;
+
+        item.newimg.style.left = item.position.dx + 'px';
+        item.newimg.style.top = item.position.dy + 'px';
+        })
+        setTimeout(update, 20);
+    }
+
+    
+
+    function makeOne() {
+        pacMen.push(makePac()); // add a new PacMan
+    }
+
+  
+    function checkCollisions(item) {
+       if (item.position.dx + item.velocity.x + item.newimg.width > p.right ||
+       item.position.dx + item.velocity.x < p.left) item.velocity.x = -item.velocity.x;
+       if (item.position.dy + item.velocity.y + item.newimg.height > p.bottom||
+       item.position.dy + item.velocity.y < p.top) item.velocity.y = -item.velocity.y;
+    }
+    
